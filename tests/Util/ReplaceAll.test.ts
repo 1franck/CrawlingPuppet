7718471ffeee568e2948833016710c4871669f83
Franck <snake386@hotmail.com>
2019-07-21 18:15:45 -0400

    added fn replaceAllArray

--- tests/Util/ReplaceAll.test.ts
+++ tests/Util/ReplaceAll.test.ts
@@ -1,11 +1,17 @@
 const { expect } = require('chai');
 import { suite, test, slow, timeout } from "mocha-typescript";
-import {replaceAll} from "../../src/Core/Util/ReplaceAll";
+import {replaceAll, replaceAllArray} from "../../src/Core/Util/ReplaceAll";
 
 @suite class ReplaceAllTest {
-    @test default() {
+    @test replaceAllFn() {
         let originalString = 'aaa - aaa - aaa - aa1';
         let expectedString = 'bbb - bbb - bbb - aa1';
         expect(replaceAll(originalString, 'aaa', 'bbb')).equal(expectedString);
+    }
+
+    @test replaceAllArrayFn() {
+        let originalString = 'aaa - aaa - aaa - aa1';
+        let expectedString = 'bbb % bbb % bbb % aa1';
+        expect(replaceAllArray(originalString, ['aaa', ' - '], ['bbb', ' % '])).equal(expectedString);
     }
 }
