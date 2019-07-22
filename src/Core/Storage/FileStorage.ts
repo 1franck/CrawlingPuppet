f9f291f1c6cbc5431d4cee2b054368ee167dead6
Franck <snake386@hotmail.com>
2019-07-21 21:02:04 -0400

    create path folder if don't exists

--- src/Core/Storage/FileStorage.ts
+++ src/Core/Storage/FileStorage.ts
@@ -1,9 +1,14 @@
 import * as fs from "fs";
 import {Storage} from "../CrawlingPuppet";
+import {fileExists} from "../Util/FileExists";
 
 export class FileStorage implements Storage
 {
     constructor(private path: string) {}
+
+    setPath(path: string) {
+        this.path = path;
+    }
 
     save(name: string, content: string, metadata: any): any {
 
@@ -11,13 +16,25 @@ export class FileStorage implements Storage
         if (metadata.url !== undefined) {
             content2Save = '|||||||||' + JSON.stringify(metadata) + '|||||||||' + "\n" + content;
         }
+        this.saveFile(name, content2Save);
+    }
+
+    private saveFile(name: string, content: string) {
+
         let filepath = this.path + '/' + name;
 
-        fs.writeFile(filepath, content2Save, (err) => {
-            // throws an error, you could also catch it here
-            if (err) throw err;
-            // success case, the file was saved
-            console.log('Content saved under ' + filepath);
+        if (!fileExists(this.path)) {
+            fs.mkdir(this.path,(err) => {
+                if(err) {
+                    throw err;
+                }
+            });
+        }
+
+        fs.writeFile(filepath, content, (err) => {
+            if (err) {
+                throw err;
+            }
         });
     }
 }
