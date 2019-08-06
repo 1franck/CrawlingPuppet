b3147a13f40ae94eb9c9832aeab13762f6115961
Franck <snake386@hotmail.com>
2019-08-06 10:42:48 -0400

    removed in favor of command-line-args

--- src/Core/Cli.ts
+++
@@ -1,9 +0,0 @@
-export class Cli
-{
-    static getUrl(): string {
-        if (process.argv[2] === undefined) {
-            throw Error('Specify an url to crawl ...');
-        }
-        return process.argv[2]
-    }
-}
